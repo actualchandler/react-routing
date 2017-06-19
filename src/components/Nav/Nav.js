f3a5c4fe0ecc29691ab9b4c7913650c1087f0d26
actualChandler <chambler97@gmail.com>
2017-06-19 12:00:02 -0600

    initial commit

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 
 import './Nav.css';
+
+
 
 import ShoppingCart from 'react-icons/lib/fa/shopping-cart';
 
@@ -9,11 +12,15 @@ function Nav( { items } ) {
   return (
     <div id="Nav__container">
       <div id="Nav__linksContainer">
+        <Link to="/" className="navLink">
         <span className="Nav__label">Store</span>
+        </Link>
 
         <div id="Nav__checkoutContainer">
           <div id="Nav__itemsInCart"> { items } </div>
+          <Link to="/checkout" className="navLink">
           <ShoppingCart id="Nav__shoppingCart" />
+          </Link>
         </div>
       </div>
     </div>
